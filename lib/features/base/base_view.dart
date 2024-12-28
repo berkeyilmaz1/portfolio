53287cbce74d250a8bc386c94ceb33c0bf5fecdb
berke.yz <berkeyilmaz189@gmail.com>
2024-12-28 09:10:31 +0300

    github actions test with placeholder

--- lib/features/base/base_view.dart
+++ lib/features/base/base_view.dart
@@ -26,6 +26,7 @@ class _BaseViewState extends State<BaseView> {
           child: Column(
             children: [
               HomeView(),
+              Placeholder(),
             ],
           )),
     );
